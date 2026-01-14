8807f203cc92e14bf3bbbfc56af90d1924990fd9
juuunaa <proletermendes@gmail.com>
2026-01-14 10:00:17 +0700

    Create AboutUs.jsx

---
+++ AboutUs.jsx
@@ -0,0 +1,13 @@
+const AboutUs = () => {
+  return (
+    <div className="about-us">
+      <h2>Tentang Paradise Nursery</h2>
+      <p>
+        Paradise Nursery adalah perusahaan yang menyediakan berbagai tanaman hias
+        berkualitas tinggi untuk mempercantik rumah dan lingkungan Anda.
+      </p>
+    </div>
+  );
+};
+
+export default AboutUs;
